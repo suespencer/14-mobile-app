e4500b4b3cd4cfaf7ea3cc4889635ff058a8cc6f
suespencer <79225260+suespencer@users.noreply.github.com>
2024-01-03 21:46:36 +0000

    After list elements styled
    Using flex: wrap

--- index.js
+++ index.js
@@ -3,27 +3,57 @@ Challenge:
 Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
 */
 
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
-    databaseURL: "https://playground-2c32f-default-rtdb.europe-west1.firebasedatabase.app/"
-}
+  databaseURL:
+    "https://realtime-database-60d98-default-rtdb.europe-west1.firebasedatabase.app/",
+};
 
-const app = initializeApp(appSettings)
-const database = getDatabase(app)
-const moviesInDB = ref(database, "movies")
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
 
-console.log(app)
+console.log(app);
 
 const addBtn = document.getElementById("add-button");
-
 const inputFieldEl = document.getElementById("input-field");
+const shoppingListEl = document.getElementById("shopping-list");
+const booksInDB = ref(database, "books");
+
+onValue(shoppingListInDB, function (snapshot) {
+  let shoppingListArray = Object.values(snapshot.val());
+  console.log(shoppingListArray);
+  clearShoppingListEl();
+
+  for (let i = 0; i < shoppingListArray.length; i++) {
+    console.log(shoppingListArray[i]);
+    appendItemToShoppingListEl(shoppingListArray[i]);
+  }
+});
+
+function clearInputFieldEl() {
+  inputFieldEl.value = "";
+}
+
+function appendItemToShoppingListEl(inputValue) {
+  shoppingListEl.innerHTML += `<li>${inputValue}</li>`;
+}
+
+function clearShoppingListEl() {
+  shoppingListEl.innerHTML = "";
+}
 
 addBtn.addEventListener("click", function () {
-    let inputValue = inputFieldEl.value
-    push(moviesInDB, inputValue)
-
-    console.log(`${inputValue} added to database`)
+  let inputValue = inputFieldEl.value;
+  push(shoppingListInDB, inputValue);
+  console.log(`${inputValue} added to database`);
+  // appendItemToShoppingListEl(inputValue)
+  clearInputFieldEl();
 });
